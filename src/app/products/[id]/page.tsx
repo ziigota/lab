baebe39d09ba02b7034dba80186486d5a1fa135e
ziigota <angela077542206@gmail.com>
2026-02-24 21:47:00 +0200

    lab3

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,11 +1,39 @@
-export default async function ProductPage({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
+import { cache } from 'react'
+import type { Metadata } from 'next'
+type Post = {
+    userId: number
+    id: number
+    title: string
+    body: string
+}
+type Props = {
+    params: Promise<{ id: string }>
+}
+const getPost = cache(async (id: string): Promise<Post> => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    if (!res.ok) throw new Error('Failed to fetch post')
+    return res.json()
+})
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const { id } = await params
+    const post = await getPost(id)
+
+    return {
+        title: post.title,
+        description: post.body,
+    }
+}
+
+export default async function PostPage({ params }: Props) {
+    const { id } = await params
+    const post = await getPost(id)
 
     return (
-        <main>
-            <h1>Страница продукта</h1>
-            <p>Идентификатор продукта: {id}</p>
-            <a href={`/products/${id}/comments`}>Перейти к комментариям</a>
-        </main>
-    );
+        <div style={{ marginTop: '30px', padding: '0 20px' }}>
+            <h1>{post.title}</h1>
+            <p style={{ color: '#fff', fontSize: '20px', marginTop: '24px' }}>
+                {post.body}
+            </p>
+        </div>
+    )
 }
